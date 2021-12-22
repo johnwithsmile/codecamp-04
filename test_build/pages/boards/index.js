c1c2acb6d7f8b8bcbdaf745075c66ef7cd931d6b
johnwithsmile <john_with_smile@kakao.com>
2021-12-22 14:07:08 +0900

    typo killed

--- test_build/pages/boards/index.js
+++ test_build/pages/boards/index.js
@@ -4,13 +4,13 @@ export default function BoardsPage() {
   return (
     <>
       <Head>
-        <meata property="og:title" content="나만의 사이트 게시판!!!" />
-        <meata property="og:url" content="http://johnwithsmile.me" />
-        <meata
+        <meta property="og:title" content="나만의 사이트 게시판!!!" />
+        <meta property="og:url" content="http://johnwithsmile.me" />
+        <meta
           property="og:image"
           content="https://cdn.cnn.com/cnnnext/dam/assets/190517103414-01-grumpy-cat-file-restricted.jpg"
         />
-        <meata
+        <meta
           property="og:description"
           content="안녕하세요 불만인 고양이 사이트입니다"
         />
